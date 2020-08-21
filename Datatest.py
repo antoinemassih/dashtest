e86fe0fbfa2f11713085def3c1d56244b7853f00
antoinemassih <antoine.amassih@gmail.com>
2020-08-21 12:02:38 +0400

    Completed TA-Lib Candlestick.py - Engulfing Candles

--- Datatest.py
+++ Datatest.py
@@ -1,11 +1,17 @@
 from datetime import datetime
 from DataConnection.StockData import StockDataFrame as SDF
 from DataConnection.PandasMySQl import DFDBObject as SDFDB
+from TechAnalysis.Candlestick import CandlePattern as cpattern
 
 StockData = SDF('AAPL')
 StockData.get_historical(datetime(2018, 1, 1), datetime(2020, 1, 1))
 
 df = StockData.data
-dataconnection = SDFDB(df)
-dataconnection.SendToDB()
+engulfing = cpattern()
+engulfing.engulfing(df)
+print(engulfing.bearishCandles)
+#dataconnection = SDFDB(df)
+#dataconnection.SendToDB()
+
+
 
--- TechAnalysis/Candlestick.py
+++ TechAnalysis/Candlestick.py
@@ -4,20 +4,18 @@ import pandas as pd
 
 class CandlePattern:
     data = pd.DataFrame()
+    bearishCandles = []
+    bullishCandles = []
+    pattern = ''
 
-    def engulfing(self,data):
+    def engulfing(self, data):
         self.data = data
-        candles = talib.CDLENGULFING(df['open'], df['high'], df['low'], df['close'])
-        for c in candles:
-            if c!=0:
+        self.pattern = 'engulfing'
+        candles = talib.CDLENGULFING(data['open'], data['high'], data['low'], data['close'])
+        for index,c in enumerate(candles):
+            if c == 100:
+                self.bullishCandles.append(index)
+            if c == -100:
+                self.bearishCandles.append(index)
 
-
-
-
-
-
-for c in candles:
-        print(c)
-
-#CDLENGULFING
-
+# CDLENGULFING
